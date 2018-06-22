f7fcf06368eddf8ae30cf950e27f58c5d19f027b
jongwook <jkim@iona.edu>
2018-06-22 15:55:10 -0400

    added visit(simplename) and visit(methoddeclaration)

--- MiMr/src/p/actions/ASTVisitorEx.java
+++ MiMr/src/p/actions/ASTVisitorEx.java
@@ -1,8 +1,22 @@
 package p.actions;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.SimpleName;
 
 public class ASTVisitorEx extends ASTVisitor {
+	
+	public boolean visit(SimpleName node) {
+		System.out.println(node.getIdentifier());
+		return true;
+	}
+	
+	
+	public boolean visit(MethodDeclaration node) {
+		
+		System.out.println(node.getName().getFullyQualifiedName());
+		
+		return true;
+	}
 	
 }
